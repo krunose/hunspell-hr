3786c2556e2202f8ce2905affcd0dc71119f9834
krunose <kruno.se@gmx.com>
2018-03-26 01:29:18 +0200

    Small improvements to description

--- tools/stdwl.php
+++ tools/stdwl.php
@@ -9,17 +9,22 @@
 
 		stdwl.php => sort to-do word list
 
-		Simple script for sorting words that are collected in auxiliary file so maintainer of a dictionary can keep track of what's added, deleted or what yet needs to be done.
+		Simple script for sorting words that are collected in auxiliary file so maintainer of a dictionary can keep track of what's added, deleted or what yet needs to be done. For file with content
 
-		Script reads the first two characters of such file and sorts lines by those two characters in following order:
+			- word2
+			-- word1
+			+- word4
+			+ word 3
+
+		script will read the first two characters of such file and sorts lines by those two characters in following order:
 
 			[1] -- word1
 			[2] - word2
 			[3] + word3
 			[4] +- word4
 
-		[1] is two minus signs indicating a word that should be deleted from dictionary but it's not yet
-		[2] minus sign followed by space indicates word that should be added to dictionary but it's not yet added
+		[1] is two minus signs indicating a word that should be removed from dictionary but it's not yet
+		[2] minus sign followed by space indicates word that should be added to dictionary but it's not yet
 		[3] plus and minus sign indicates word that was initially marked [1] but now it's removed from dictionary (completed)
 		[4] plus followed by space indicates that word that was initially marked [2], now is added to dictionary.
 
